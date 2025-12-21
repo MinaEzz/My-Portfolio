a6eb5d4ce94581c0c2e78f75686c7fdff1447ef3
Mina Ezzat <minaezzat98@gmail.com>
2025-12-21 18:27:01 +0200

    fix: sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,14 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  // const baseUrl = "https://dev-mina-ezzat.vercel.app/";
+  const baseUrl = "https://dev-mina-ezzat.vercel.app";
+
   return {
     rules: {
       userAgent: "*",
       allow: ["/", "/about", "/projects", "/contact"],
       disallow: ["/api/*"],
     },
-    sitemap: "https://dev-mina-ezzat.vercel.app/sitemap.xml",
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
