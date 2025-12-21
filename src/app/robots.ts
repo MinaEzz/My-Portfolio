240d1b26db3b2cd4da74f279f2625365175f48a8
Mina Ezzat <minaezzat98@gmail.com>
2025-12-21 18:02:35 +0200

    fix: sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,13 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://dev-mina-ezzat.vercel.app/";
-
+  // const baseUrl = "https://dev-mina-ezzat.vercel.app/";
   return {
     rules: {
       userAgent: "*",
       allow: ["/", "/about", "/projects", "/contact"],
       disallow: ["/api/*"],
     },
-    sitemap: `${baseUrl}sitemap.xml`,
+    sitemap: "https://dev-mina-ezzat.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,28 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://dev-mina-ezzat.vercel.app/";
-
+  // const baseUrl = "https://dev-mina-ezzat.vercel.app/";
   return [
     {
-      url: baseUrl,
+      url: "https://dev-mina-ezzat.vercel.app/",
       priority: 1,
       changeFrequency: "yearly",
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}about`,
+      url: "https://dev-mina-ezzat.vercel.app/about",
       priority: 0.8,
       changeFrequency: "yearly",
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}projects`,
+      url: "https://dev-mina-ezzat.vercel.app/projects",
       priority: 0.8,
       changeFrequency: "monthly",
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}contact`,
+      url: "https://dev-mina-ezzat.vercel.app/contact",
       priority: 0.6,
       changeFrequency: "yearly",
       lastModified: new Date(),
